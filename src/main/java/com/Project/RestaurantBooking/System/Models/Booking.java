d90a1a08e6b4eee16aab73ceb5c54b103535766a
kiranqureshi1 <kiranqureshi@outlook.com>
2019-04-19 18:45:49 +0100

    booking model ready

--- src/main/java/com/Project/RestaurantBooking/System/Models/Booking.java
+++ src/main/java/com/Project/RestaurantBooking/System/Models/Booking.java
@@ -1,7 +1,10 @@
 package com.Project.RestaurantBooking.System.Models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import javax.persistence.*;
 import javax.persistence.Table;
+import java.awt.print.Book;
 
 @Entity
 @Table(name = "bookings")
@@ -14,4 +17,79 @@ public class Booking {
     @Column
     private int time;
 
+    @Column
+    private int date;
+
+    @JsonIgnoreProperties("bookings")
+    @ManyToOne
+    @JoinColumn(name = "customer_id", nullable = false)
+    private Customer customer;
+
+    @JsonIgnoreProperties("bookings")
+    @ManyToOne
+    @JoinColumn(name = "table_id", nullable = false)
+    private Table table;
+
+    @Column
+    private Reciept reciept;
+
+    public Booking(int time, int date, Customer customer, Table table, Reciept reciept) {
+        this.time = time;
+        this.date = date;
+        this.customer = customer;
+        this.table = table;
+        this.reciept = reciept;
+    }
+
+    public Booking(){
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+
+    public int getDate() {
+        return date;
+    }
+
+    public void setDate(int date) {
+        this.date = date;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public Table getTable() {
+        return table;
+    }
+
+    public void setTable(Table table) {
+        this.table = table;
+    }
+
+    public Reciept getReciept() {
+        return reciept;
+    }
+
+    public void setReciept(Reciept reciept) {
+        this.reciept = reciept;
+    }
 }
