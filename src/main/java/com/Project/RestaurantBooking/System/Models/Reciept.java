3fd2e758579b012c12c04e16ac779a7fc29e4bc2
kiranqureshi1 <kiranqureshi@outlook.com>
2019-04-19 18:57:50 +0100

    properties of table added

--- src/main/java/com/Project/RestaurantBooking/System/Models/Reciept.java
+++ src/main/java/com/Project/RestaurantBooking/System/Models/Reciept.java
@@ -1,4 +1,40 @@
 package com.Project.RestaurantBooking.System.Models;
 
+import javax.persistence.*;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "reciepts")
 public class Reciept {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private int amount;
+
+    public Reciept(int amount) {
+        this.amount = amount;
+    }
+
+    public Reciept(){
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
 }
--- src/main/java/com/Project/RestaurantBooking/System/Models/Table.java
+++ src/main/java/com/Project/RestaurantBooking/System/Models/Table.java
@@ -1,4 +1,22 @@
 package com.Project.RestaurantBooking.System.Models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.util.List;
+
+
+@Entity
+@javax.persistence.Table(name = "tables")
 public class Table {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "booking", fetch = FetchType.LAZY)
+    private List<Booking> bookings;
+
+
 }
