ebb010042c65bafc9feccb782282c2707a616c0b
kiranqureshi1 <kiranqureshi@outlook.com>
2019-04-19 19:51:39 +0100

    dataLoader is all setup

--- src/main/java/com/Project/RestaurantBooking/System/Components/DataLoader.java
+++ src/main/java/com/Project/RestaurantBooking/System/Components/DataLoader.java
@@ -1,4 +1,32 @@
 package com.Project.RestaurantBooking.System.Components;
 
-public class DataLoader {
+import com.Project.RestaurantBooking.System.Models.ResturantTable;
+import com.Project.RestaurantBooking.System.Repositories.BookingRepositories.BookingRepository;
+import com.Project.RestaurantBooking.System.Repositories.CustomerRepositories.CustomerRepository;
+import com.Project.RestaurantBooking.System.Repositories.RecieptRepositories.RecieptRepository;
+import com.Project.RestaurantBooking.System.Repositories.ResturantTableRepositories.ResturantTableRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements ApplicationRunner {
+
+    @Autowired
+    BookingRepository bookingRepository;
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Autowired
+    ResturantTableRepository resturantTableRepository;
+
+    @Autowired
+    RecieptRepository recieptRepository;
+
+    public DataLoader(){
+
+    }
+
+
 }
