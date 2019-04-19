13f724a6a4e535c95d8866d6040e358b3ce8185d
kiranqureshi1 <kiranqureshi@outlook.com>
2019-04-19 20:21:15 +0100

    controllers annotation are all set

--- src/main/java/com/Project/RestaurantBooking/System/Controllers/ResturantTableController.java
+++ src/main/java/com/Project/RestaurantBooking/System/Controllers/ResturantTableController.java
@@ -5,6 +5,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/bookings")
-public class BookingController {
+@RequestMapping(value = "/resturantTables")
+public class ResturantTableController {
 }
--- src/main/java/com/Project/RestaurantBooking/System/Controllers/TableController.java
+++
@@ -1,4 +0,0 @@
-package com.Project.RestaurantBooking.System.Controllers;
-
-public class TableController {
-}
