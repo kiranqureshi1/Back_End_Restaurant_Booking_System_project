62f65cdd7f2e3f70ac1659e5c58e0c82d36b6146
kiranqureshi1 <kiranqureshi@outlook.com>
2019-04-19 19:59:16 +0100

    main repositories are all set

--- src/main/java/com/Project/RestaurantBooking/System/Components/DataLoader.java
+++ src/main/java/com/Project/RestaurantBooking/System/Components/DataLoader.java
@@ -1,11 +1,13 @@
 package com.Project.RestaurantBooking.System.Components;
 
+import com.Project.RestaurantBooking.System.Models.Customer;
 import com.Project.RestaurantBooking.System.Models.ResturantTable;
 import com.Project.RestaurantBooking.System.Repositories.BookingRepositories.BookingRepository;
 import com.Project.RestaurantBooking.System.Repositories.CustomerRepositories.CustomerRepository;
 import com.Project.RestaurantBooking.System.Repositories.RecieptRepositories.RecieptRepository;
 import com.Project.RestaurantBooking.System.Repositories.ResturantTableRepositories.ResturantTableRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
@@ -26,6 +28,11 @@ public class DataLoader implements ApplicationRunner {
 
     public DataLoader(){
 
+    }
+
+    public void run(ApplicationArguments args){
+        Customer customer1 = new Customer("John");
+        customerRepository.save(customer1);
     }
 
 
--- src/main/java/com/Project/RestaurantBooking/System/Repositories/BookingRepositories/BookingRepository.java
+++ src/main/java/com/Project/RestaurantBooking/System/Repositories/BookingRepositories/BookingRepository.java
@@ -1,4 +1,10 @@
 package com.Project.RestaurantBooking.System.Repositories.BookingRepositories;
 
-public interface BookingRepository {
+
+import com.Project.RestaurantBooking.System.Models.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookingRepository extends JpaRepository<Booking, Long>, BookingRepositoryCustom{
 }
