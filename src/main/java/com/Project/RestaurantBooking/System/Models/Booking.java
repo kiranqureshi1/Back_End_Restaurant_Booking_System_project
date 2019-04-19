32905d95db6293bb5f734294afd7f89b73c0c49e
kiranqureshi1 <kiranqureshi@outlook.com>
2019-04-19 18:38:14 +0100

    customer model ready

--- src/main/java/com/Project/RestaurantBooking/System/Models/Booking.java
+++ src/main/java/com/Project/RestaurantBooking/System/Models/Booking.java
@@ -1,4 +1,17 @@
 package com.Project.RestaurantBooking.System.Models;
 
+import javax.persistence.*;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "bookings")
 public class Booking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private int time;
+
 }
--- src/main/java/com/Project/RestaurantBooking/System/Models/Customer.java
+++ src/main/java/com/Project/RestaurantBooking/System/Models/Customer.java
@@ -1,4 +1,55 @@
 package com.Project.RestaurantBooking.System.Models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import javax.persistence.Table;
+import java.util.List;
+
+@Entity
+@Table(name = "customers")
 public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "customer", fetch = FetchType.LAZY)
+    private List<Booking> bookings;
+
+    public Customer(String name) {
+        this.name = name;
+    }
+
+    public Customer(){
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Booking> getBookings() {
+        return bookings;
+    }
+
+    public void setBookings(List<Booking> bookings) {
+        this.bookings = bookings;
+    }
 }
