96acb7b42ff1b15fb0342006c50170118e0dd907
kiranqureshi1 <kiranqureshi@outlook.com>
2019-04-19 19:00:15 +0100

    getter, setters and constructoer to the table model added

--- src/main/java/com/Project/RestaurantBooking/System/Models/Table.java
+++ src/main/java/com/Project/RestaurantBooking/System/Models/Table.java
@@ -18,5 +18,38 @@ public class Table {
     @OneToMany(mappedBy = "booking", fetch = FetchType.LAZY)
     private List<Booking> bookings;
 
+    @Column
+    private int tableNumber;
 
+    public Table(int tableNumber) {
+        this.tableNumber = tableNumber;
+    }
+
+    public Table(){
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<Booking> getBookings() {
+        return bookings;
+    }
+
+    public void setBookings(List<Booking> bookings) {
+        this.bookings = bookings;
+    }
+
+    public int getTableNumber() {
+        return tableNumber;
+    }
+
+    public void setTableNumber(int tableNumber) {
+        this.tableNumber = tableNumber;
+    }
 }
