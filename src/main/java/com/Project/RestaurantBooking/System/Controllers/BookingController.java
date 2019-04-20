699a373a06fa8ff4a6aed103e2328220144644c2
kiranqureshi1 <kiranqureshi@outlook.com>
2019-04-20 10:37:51 +0100

    route of derived booking query added to the booking controller

--- src/main/java/com/Project/RestaurantBooking/System/Controllers/BookingController.java
+++ src/main/java/com/Project/RestaurantBooking/System/Controllers/BookingController.java
@@ -1,10 +1,25 @@
 package com.Project.RestaurantBooking.System.Controllers;
 
 
+import com.Project.RestaurantBooking.System.Models.Booking;
+import com.Project.RestaurantBooking.System.Repositories.BookingRepositories.BookingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.awt.print.Book;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/bookings")
 public class BookingController {
+
+    @Autowired
+    BookingRepository bookingRepository;
+
+    @GetMapping(value = "/date/{date}")
+    public List<Booking> findBookingForGivenDate(int date){
+        return bookingRepository.findBookingByDate(date);
+    }
 }
