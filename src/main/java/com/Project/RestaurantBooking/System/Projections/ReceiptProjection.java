d8bd0a790357eef6501a4bc2d70e1b2dd1009cc0
kiranqureshi1 <kiranqureshi@outlook.com>
2019-04-21 15:21:27 +0100

    reciept projection added

--- src/main/java/com/Project/RestaurantBooking/System/Projections/ReceiptProjection.java
+++ src/main/java/com/Project/RestaurantBooking/System/Projections/ReceiptProjection.java
@@ -1,4 +1,13 @@
 package com.Project.RestaurantBooking.System.Projections;
 
-public class ReceiptProjection {
+
+import com.Project.RestaurantBooking.System.Models.Booking;
+import com.Project.RestaurantBooking.System.Models.Receipt;
+import org.springframework.data.rest.core.config.Projection;
+
+@Projection(name= "embedBooking", types = Receipt.class)
+public interface ReceiptProjection {
+    long getId();
+    int getAmount();
+    Booking getBooking();
 }
