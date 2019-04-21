d6b2854923dc8bbfd3c8f24e5e48c7bea3497696
kiranqureshi1 <kiranqureshi@outlook.com>
2019-04-21 15:43:41 +0100

    customer repo updated with customerProjection

--- src/main/java/com/Project/RestaurantBooking/System/Repositories/CustomerRepositories/CustomerRepository.java
+++ src/main/java/com/Project/RestaurantBooking/System/Repositories/CustomerRepositories/CustomerRepository.java
@@ -2,12 +2,15 @@ package com.Project.RestaurantBooking.System.Repositories.CustomerRepositories;
 
 import com.Project.RestaurantBooking.System.Models.Booking;
 import com.Project.RestaurantBooking.System.Models.Customer;
+import com.Project.RestaurantBooking.System.Projections.BookingProjection;
+import com.Project.RestaurantBooking.System.Projections.CustomerProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Repository
+@RepositoryRestResource(excerptProjection = CustomerProjection.class)
 public interface CustomerRepository extends JpaRepository<Customer, Long>, CustomerRepositoryCustom {
 }
