f0c5f113953705e391fc7e815707fd4d732c4df8
kiranqureshi1 <kiranqureshi@outlook.com>
2019-04-21 15:16:16 +0100

    Booking projection added

--- src/main/java/com/Project/RestaurantBooking/System/Projections/BookingProjection.java
+++ src/main/java/com/Project/RestaurantBooking/System/Projections/BookingProjection.java
@@ -1,4 +1,16 @@
 package com.Project.RestaurantBooking.System.Projections;
 
-public class BookingProjection {
+
+import com.Project.RestaurantBooking.System.Models.Booking;
+import com.Project.RestaurantBooking.System.Models.Customer;
+import com.Project.RestaurantBooking.System.Models.RestaurantTable;
+import org.springframework.data.rest.core.config.Projection;
+
+@Projection(name= "embedCustomer", types = Booking.class)
+public interface BookingProjection {
+    long getId();
+    int getTime();
+    int getDate();
+    Customer getCustomer();
+    RestaurantTable getRestaurantTable();
 }
