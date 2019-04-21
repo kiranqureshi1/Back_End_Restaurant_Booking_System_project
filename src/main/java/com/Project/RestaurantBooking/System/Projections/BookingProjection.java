2e047b503ae2f363823bee350398c7895cf4f931
kiranqureshi1 <kiranqureshi@outlook.com>
2019-04-21 15:18:43 +0100

    reciept to the booking projection added

--- src/main/java/com/Project/RestaurantBooking/System/Projections/BookingProjection.java
+++ src/main/java/com/Project/RestaurantBooking/System/Projections/BookingProjection.java
@@ -3,6 +3,7 @@ package com.Project.RestaurantBooking.System.Projections;
 
 import com.Project.RestaurantBooking.System.Models.Booking;
 import com.Project.RestaurantBooking.System.Models.Customer;
+import com.Project.RestaurantBooking.System.Models.Receipt;
 import com.Project.RestaurantBooking.System.Models.RestaurantTable;
 import org.springframework.data.rest.core.config.Projection;
 
@@ -13,4 +14,5 @@ public interface BookingProjection {
     int getDate();
     Customer getCustomer();
     RestaurantTable getRestaurantTable();
+    Receipt getReciept();
 }
